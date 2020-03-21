260266c848d9b8257e01c0860e80f63a67133204
gutop84 <gutop84@mail.ru>
2020-03-21 14:28:20 +0300

    Теперь с некрасивыми тестами.

--- src/test/java/ru/liga/intership/badcode/BadcodeApplicationTests.java
+++ src/test/java/ru/liga/intership/badcode/BadcodeApplicationTests.java
@@ -11,58 +11,68 @@ import ru.liga.intership.badcode.service.PersonService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BadcodeApplicationTests {
-	PersonService personService;
+    PersonService personService;
 
-	@Before
-	public void init(){
-		personService = new PersonService();
-	}
+    @Before
+    public void init() {
+        personService = new PersonService();
+    }
 
-	@Test
-	public void wrongConnectToDataBaseTest1() {
-		Assertions.assertThatThrownBy(()->personService.connectToDataBase("blablabla", "sa", ""));
-	}
-	@Test
-	public void wrongConnectToDataBaseTest2() {
-		Assertions.assertThatThrownBy(()->personService.connectToDataBase("jdbc:hsqldb:mem:test", "blablabla", ""));
-	}
-	@Test
-	public void wrongConnectToDataBaseTest3() {
-		Assertions.assertThatThrownBy(()->personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "blablabla"));
-	}
+    @Test
+    public void wrongConnectToDataBaseTest1() {
+        Assertions.assertThatThrownBy(() -> personService.connectToDataBase("blablabla", "sa", ""));
+    }
 
-	@Test
-	public void wrongSQL_Query1() {
-		personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
-		Assertions.assertThatThrownBy(()->personService.doQuery("blablabla"));
-	}
+    @Test
+    public void wrongConnectToDataBaseTest2() {
+        Assertions.assertThatThrownBy(() -> personService.connectToDataBase("jdbc:hsqldb:mem:test", "blablabla", ""));
+    }
 
-	@Test
-	public void wrongSQL_Query2() {
-		personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
-		Assertions.assertThatThrownBy(()->personService.doQuery("SELECT * FROM person WHERE sex = blablabla AND age > 18"));
-	}
+    @Test
+    public void wrongConnectToDataBaseTest3() {
+        Assertions.assertThatThrownBy(() -> personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "blablabla"));
+    }
 
-	@Test
-	public void wrongSQL_Query3() {
-		personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
-		Assertions.assertThatThrownBy(()->personService.doQuery("SELECT * FROM blablabla WHERE sex = 'male' AND age > 18"));
-	}
+    @Test
+    public void wrongSQL_Query1() {
+        personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
+        Assertions.assertThatThrownBy(() -> personService.doQuery("blablabla"));
+    }
 
-	@Test
-	public void methodMainTest(){
-		personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
-		personService.doQuery("SELECT * FROM person WHERE sex = 'male' AND age > 18");
+    @Test
+    public void wrongSQL_Query2() {
+        personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
+        Assertions.assertThatThrownBy(() -> personService.doQuery("SELECT * FROM person WHERE sex = blablabla AND age > 18"));
+    }
 
-		Assertions.assertThat(personService.getSelectedPersonsAverageBMI()).isEqualTo(25.774209960992707d);
-	}
+    @Test
+    public void wrongSQL_Query3() {
+        personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
+        Assertions.assertThatThrownBy(() -> personService.doQuery("SELECT * FROM blablabla WHERE sex = 'male' AND age > 18"));
+    }
 
-	@Test
-	public void zeroPersonTest(){
-		personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
-		personService.doQuery("SELECT * FROM person WHERE sex = 'male' AND age > 1800");
+    @Test
+    public void doSQL_QueryBeforeConnect() {
+        Assertions.assertThatThrownBy(() -> {
+            personService.doQuery("SELECT * FROM person WHERE sex = 'male' AND age > 1800");
+            personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
+        });
+    }
 
-		Assertions.assertThat(personService.getSelectedPersonsAverageBMI()).isEqualTo(0);
-	}
+    @Test
+    public void methodMainTest() {
+        personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
+        personService.doQuery("SELECT * FROM person WHERE sex = 'male' AND age > 18");
+
+        Assertions.assertThat(personService.getSelectedPersonsAverageBMI()).isEqualTo(25.774209960992707d);
+    }
+
+    @Test
+    public void zeroPersonTest() {
+        personService.connectToDataBase("jdbc:hsqldb:mem:test", "sa", "");
+        personService.doQuery("SELECT * FROM person WHERE sex = 'male' AND age > 1800");
+
+        Assertions.assertThat(personService.getSelectedPersonsAverageBMI()).isEqualTo(0);
+    }
 
 }
